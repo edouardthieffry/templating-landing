2d1698c9339373df972abb9a2756aa5217c46dfe
edouardthieffry <e.thieffry62930@gmail.com>
2017-02-15 22:33:20 +0100

    create folder js & link to firebase database / create back-office folder

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+$(function () {
+    //firebase config-------------------------------------------------------------------------------------------------------
+    var config = {
+        apiKey: "AIzaSyBPDGAaUMql43KclToTWddkvZnYSuhblAg",
+        authDomain: "templating-f0351.firebaseapp.com",
+        databaseURL: "https://templating-f0351.firebaseio.com",
+        storageBucket: "templating-f0351.appspot.com",
+        messagingSenderId: "239269448680"
+    };
+    firebase.initializeApp(config);
+});
